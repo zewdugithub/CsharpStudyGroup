d08cfb20189102dcc21e73926cc69d3138d53eb2
zewdugithub <95198898+zewdugithub@users.noreply.github.com>
2022-11-27 21:37:56 -0500

    Add project files.

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Console.Write(i +" , ");
+
+            //}
+            //Console.ReadLine();
+
+            /* Write a C# Sharp program that takes a number as input and print its
+            multiplication table
+            o Test Data:
+            Enter the number: 5
+            Expected Output:
+            5 * 0 = 0
+            5 * 1 = 5
+            5 * 2 = 10
+            5 * 3 = 15....5 * 10 = 50
+            */
+
+            //int num1;
+            //int num2;
+            //int result;
+            //Console.WriteLine(" please enter the first number");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" please enter the second number");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //result = num1 * num2;
+            //Console.WriteLine(" The result is {0}", result);
+            //Console.WriteLine ();
+            //Console.ReadLine();
+
+            /*
+             Write a C# Sharp program that takes a number and a width also a
+            number, as input and then displays a triangle of that width, using
+            that number.
+            Test Data
+            Enter a number: 6
+            Enter the desired width: 6
+            Expected Output :
+            666666
+            66666
+            6666
+            666
+            66
+            6 */
+
+            //for (int w = 6; w >0; w--)
+
+            //{ for (int j = 0; j < w; j++)
+            //    {
+            //        Console.Write("6");
+            //    }
+
+            //   Console.WriteLine();
+
+            //}
+            //console= Console.ReadLine();
+
+
+        }
+    }
+}
