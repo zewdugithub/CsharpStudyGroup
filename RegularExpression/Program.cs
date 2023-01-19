ab0611c95df8ff9e83ff16c0990cc788e8df587d
Emanaws <redwanomer44@gmail.com>
2023-01-18 23:57:58 -0600

    Regular Expression

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //615-555-4578
+            string pattern = @"(\d{3})-(\d{3})-(\d{4})";
+            Console.WriteLine("Please Enter Your Phone Number");
+            string Userphone = Console.ReadLine();
+            Match match = Regex.Match(Userphone, pattern);
+            if (match.Success)
+            {
+                Console.WriteLine("Valid Phone Number");
+            }
+            else
+            {
+                Console.WriteLine(" Not Valid Phone Number");
+
+            }
+            
+        }
+    }
+}
