ef4eff05b230a764f1cc4b64d3b35dc6818b0c55
Emanaws <redwanomer44@gmail.com>
2023-01-25 21:05:57 -0600

    Add Department Method

---
+++ Department/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Department
+{
+    internal class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public int FacultyId { get; set; }
+
+        public string DepIdEvenOrOdd(int _DepartmentId)
+        {
+            Console.WriteLine("Please Enter Enter Departmen Id?");
+            _DepartmentId = int.Parse(Console.ReadLine());
+            if (_DepartmentId % 2 == 0)
+                return "Even";
+            else
+                return "Odd";
+            
+        }
+    }
+}
---
+++ Department/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Department
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d = new Department();
+            int _DepartmentId = d.DepartmentId;
+            Console.WriteLine("The Department Id you Entered is {0}" , d.DepIdEvenOrOdd(_DepartmentId));
+            Console.ReadLine();
+        }
+    }
+}
